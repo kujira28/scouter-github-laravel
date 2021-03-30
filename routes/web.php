8e9e6942aa6fdd40e62e871e6feb8ac404b13897
wkazunori <wakamatsu.kazunori@gmail.com>
2021-03-30 20:10:42 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -22,23 +22,23 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 
 // ログインしていないユーザーはすべてログイン画面に遷移
-Route::group(['middleware' => ['auth']], function(){
-    Route::get('/' , 'HomeController@index' );
-    Route::get('/home' , 'HomeController@index' );
-    Route::get('/reservations/create' , 'ReservationController@create' )
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index');
+    Route::get('/home', 'HomeController@index');
+    Route::get('/reservations/create', 'ReservationController@create')
         ->name('reservations.create');
-    Route::post('/reservations/store' , 'ReservationController@store' )
+    Route::post('/reservations/store', 'ReservationController@store')
         ->name('reservations.store');
     Route::get('/reservations/approve/{id}', 'ReservationController@approve')
         ->name('reservations.approve');
     Route::get('/reservations/reject/{id}', 'ReservationController@reject')
         ->name('reservations.reject');
 
-    Route::get('/rooms/index' , 'MeetingRoomController@index' )
+    Route::get('/rooms/index', 'MeetingRoomController@index')
         ->name('rooms.index');
-    Route::get('/rooms/create' , 'MeetingRoomController@create' )
+    Route::get('/rooms/create', 'MeetingRoomController@create')
         ->name('rooms.create');
-    Route::post('/rooms/store' , 'MeetingRoomController@store' )
+    Route::post('/rooms/store', 'MeetingRoomController@store')
         ->name('rooms.store');
     Route::get('/rooms/edit/{id}', 'MeetingRoomController@edit')
         ->name('rooms.edit');
@@ -47,23 +47,22 @@ Route::group(['middleware' => ['auth']], function(){
     Route::get('/rooms/delete/{id}', 'MeetingRoomController@delete')
         ->name('rooms.delete');
 
-    Route::get('/users/index' , 'UserController@index' )
+    Route::get('/users/index', 'UserController@index')
         ->name('users.index');
-    Route::post('/users/index' , 'UserController@search' )
+    Route::post('/users/index', 'UserController@search')
         ->name('users.search');
-    Route::get('/users/show/{id}' , 'UserController@show' )
+    Route::get('/users/show/{id}', 'UserController@show')
         ->name('users.show');
     Route::get('/users/edit/{id}', 'UserController@edit')
         ->name('users.edit');
     Route::put('/users/update', 'UserController@update')
         ->name('users.update');
 
-    Route::get('/items/create' , 'ItemController@create' )
+    Route::get('/items/create', 'ItemController@create')
         ->name('items.create');
-    Route::post('/items/store' , 'ItemController@store' )
+    Route::post('/items/store', 'ItemController@store')
         ->name('items.store');
 });
 
 // ユーザー一覧の取得
 Route::get('/user', 'UserController@index');
-
